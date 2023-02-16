14a2814ccaec3c3377068a8b9de6ae9d58bd9dbc
josh fraim <josephefraim.galvez@gmail.com>
2023-02-16 22:18:23 +0800

    logo and favicon added

--- src/component/Page.jsx
+++ src/component/Page.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import './page.css'
-import sound from './click.mp3';
 
 const Page = () => {
     const [advice, setAdvice] = useState('');
@@ -22,22 +21,20 @@ const Page = () => {
         });
     };
 
-    const audio = new Audio('./click.mp3');
-    const playSound = () => {
-        audio(sound).play();
-    }
     const handleClick = () => {
         fetchAdvice();
     } 
 
   return (
     <div className = 'app'>
+        <div className="logo">
+            <img src="../logo.png" alt="logo" />
+        </div>
         <div className='card'>
             <h2 className='heading'>{advice}</h2>
             <button className='button' onClick={handleClick}>
                 <span> GIVE ME ADVICE </span>
             </button>
-            <button onClick={playSound}>play</button>
         </div>
     </div>
   )
